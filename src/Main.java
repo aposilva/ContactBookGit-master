8aca480aa12c5328b4bfb207bea86f7fb4c02c87
Afonso <apo.silva@campus.fct.unl.pt>
2021-10-05 17:48:02 +0100

    dfsfdfsdfdsf

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Main {
     //Constantes que definem os comandos
     public static final String ADD_CONTACT    = "AC";
-    public static final String REMOVE_CONTACT = "RC";
+    public static final String  REMOVE_CONTACT = "RC";
     public static final String GET_PHONE      = "GP";
     public static final String GET_EMAIL      = "GE";
     public static final String SET_PHONE      = "SP";
